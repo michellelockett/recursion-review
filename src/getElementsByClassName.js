2e9cadd6f93ae2653177a7a62f3fc855aef82069
Michelle Lockett <shelldog42@gmail.com>
2017-10-15 17:37:10 -0700

    Pass get elements by class name with pomander

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -1,10 +1,30 @@
 // If life was easy, we could just do things the easy way:
 // var getElementsByClassName = function (className) {
-//   return document.getElementsByClassName(className);
-// };
-
+// return document.getElementsByClassName(className);
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+var getElementsByClassName = function(className) {
+  var results = [];
+  //helper function to find the matching class name
+  var checkClassName = function(node) {
+    if (node.classList) {
+      return node.classList.contains(className);
+    }
+    return false;
+  };
+  //the recursive function
+  var findElements = function(nodes) {
+    //go through all the items in the collection
+    //if checkClassName is true, push the node to results
+    Array.prototype.forEach.call(nodes, function(node) {
+      if (checkClassName(node)) {
+        results.push(node);
+      }
+      if (node.childNodes) {
+        findElements(node.childNodes);
+      }
+    });
+  };
+
+  findElements(document.childNodes);
+  return results;
 };
